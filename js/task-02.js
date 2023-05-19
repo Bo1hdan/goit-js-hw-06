efb375ff242a5970bf4088db0d50e92cb54275c2
Bo1hdan <87267253+Bo1hdan@users.noreply.github.com>
2023-05-19 15:47:05 -0600

    2 done

--- js/task-02.js
+++ js/task-02.js
@@ -7,10 +7,14 @@ const ingredients = [
   "Condiments",
 ];
 let list = document.querySelector("#ingredients");
+let arr = [];
 
 ingredients.forEach((ingredient) => {
   let listItem = document.createElement("li");
   listItem.classList.add("item");
   listItem.innerText = ingredient;
+  arr.push(listItem);
   list.append(listItem);
 });
+
+//Я не впевнений що це правильно
