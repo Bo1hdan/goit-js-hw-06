b87c167d9950c6b616a89d5414b366cca72626e1
Bo1hdan <87267253+Bo1hdan@users.noreply.github.com>
2023-05-20 10:41:55 -0600

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -8,10 +8,10 @@ const ingredients = [
 ];
 let list = document.querySelector("#ingredients");
 
-const listOfItems = ingredients.map((ingredient) => {
+const elementOfItems = ingredients.map((ingredient) => {
   let item = document.createElement("li");
   item.classList.add("item");
   item.innerText = ingredient;
   return item;
 });
-list.append(...listOfItems);
+list.append(...elementOfItems);
